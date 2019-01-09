403927a4510d702a94bee43a3584142703505218
Björn Hempel <bjoern@hempel.li>
2019-01-09 23:46:08 +0100

    add extended csv logging

--- imagenet/__log.py
+++ imagenet/__log.py
@@ -12,6 +12,37 @@ import torch
 
 
 """
+
+"""
+def getFormatedPath(
+    path=None,
+    name='validated',
+    epoch=0,
+    time=None,
+    learning_rate=0.001,
+    momentum=0.9,
+    batch_size=256,
+    workers=4,
+    weight_decay=0.0001,
+    pretrained=True,
+    file='{}_lr{}_m{}_bs{}_w{}_wd{}_{}.{}csv'
+):
+    file = file.replace('{}', name + '_e' + str(epoch) if epoch else name, 1)
+
+    file_formated = file.format(
+        learning_rate,
+        momentum,
+        batch_size,
+        workers,
+        weight_decay,
+        'p' if pretrained else 'r',
+        '' if time is None else datetime.datetime.fromtimestamp(time.timestamp()).strftime('%Y%m%d_%H%M%S') + '.'
+    )
+
+    return os.path.join(path, file_formated) if path is not None else file_formated
+
+
+"""
 Gets the csv handler
 
 @author: Björn Hempel <bjoern@hempel.li>
@@ -22,6 +53,9 @@ Gets the csv handler
 """
 def getCSVHandler(csv_file, *values):
 
+    # create folder for csv file
+    createFolderForFile(csv_file)
+
     # move existing file to timestamp file
     if os.path.exists(csv_file):
         os.rename(csv_file, getDateName(csv_file))
@@ -29,6 +63,54 @@ def getCSVHandler(csv_file, *values):
     csv_handler = open(csv_file, 'a')
     writeCSV(csv_handler, True, *values)
     return csv_handler
+
+
+"""
+
+"""
+def getCSVHandlerWithHeader(path, name, epoch, args, time):
+
+    headers = {
+        'settings': [
+            'name', 'value'
+        ],
+        'summary': [
+            'time taken', 'train session', 'epoch number current', 'epoch number overall',
+            'current phase (train or val)', 'loss', 'accuracy in percent', 'best accuracy in percent',
+            'learning rate'
+        ],
+        'summary_full': [
+            'time taken', 'train session', 'epoch number current', 'epoch number overall',
+            'current phase (train or val)', 'current processed images', 'correct processed images',
+            'images overall', 'processed in percent', 'loss', 'accuracy in percent', 'learning rate'
+        ],
+        'validated': [
+            'time taken', 'file name', 'status', 'real class', 'predicted class 1', 'predicted class 1 (%)',
+            'predicted class 2', 'predicted class 2 (%)', 'predicted class 3', 'predicted class 3 (%)',
+            'predicted class 4', 'predicted class 4 (%)', 'predicted class 5', 'predicted class 5 (%)'
+        ]
+    }
+
+    # prepare summary csv
+    csv_path = getFormatedPath(
+        path,
+        name,
+        epoch,
+        time,
+        args.lr,
+        args.momentum,
+        args.batch_size,
+        args.workers,
+        args.weight_decay,
+        args.pretrained
+    )
+
+    header = headers[name]
+
+    return getCSVHandler(
+        csv_path,
+        *header
+    )
 
 
 """
@@ -117,6 +199,8 @@ def writeCSV(file, header, *values):
             valueStr = 'True' if value else 'False'
         elif type(value) is torch.Tensor:
             valueStr = str(value.item())
+        elif type(value) is None.__class__:
+            valueStr = 'None'
         elif type(value) is list:
             #print('ignore list elements')
             continue
@@ -132,8 +216,6 @@ def writeCSV(file, header, *values):
             csvLine += ','
 
         csvLine += valueStr
-
-    #print('Write csv line: {}'.format(csvLine))
 
     file.write(csvLine + '\n')
     file.flush()
@@ -156,3 +238,21 @@ def getNextFile(path):
             return filePath
 
         fileVersionCounter += 1
+
+"""
+Gets all properties of given class.
+
+@author  Björn Hempel <bjoern@hempel.li>
+@version 1.0 (20190106)
+@param cls: the class to determine
+@return: array of the given class
+"""
+def getClassProperties(cls):
+    return [i for i in cls.__dict__.keys() if i[:1] != '_']
+
+"""
+
+"""
+def createFolderForFile(file, mode=0o775):
+    path = os.path.dirname(file)
+    os.makedirs(path, mode, True)
--- imagenet/main.py
+++ imagenet/main.py
@@ -30,6 +30,12 @@ model_names = sorted(name for name in models.__dict__
 
 # define some global vars
 csv_handler = {}
+csv_files = {}
+time_start = datetime.datetime.now()
+accuracy_best = {
+    'train': 0,
+    'val': 0
+}
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
@@ -52,6 +58,12 @@ parser.add_argument('-b', '--batch-size', default=256, type=int,
                          'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--learning-rate-decrease-factor', default=0.1, type=float,
+                    help='learning rate decrease factor')
+parser.add_argument('--learning-rate-decrease-after', default=30, type=int,
+                    help='learning rate decrease after')
+parser.add_argument('--linear-layer', default=None, type=int,
+                    help='set a linear layer')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
 parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
@@ -84,8 +96,20 @@ parser.add_argument('--multiprocessing-distributed', action='store_true',
                          'N processes per node, which has N GPUs. This is the '
                          'fastest way to use PyTorch for either single node or '
                          'multi node data parallel training')
-parser.add_argument('--write-csv-validated', default=None, type=str,
+parser.add_argument('--session-name', default='all', type=str,
+                    help='set the session name for logging')
+parser.add_argument('--csv-path-settings', default=None, type=str,
+                    help='path to write the settings csv')
+parser.add_argument('--csv-path-summary', default=None, type=str,
+                    help='path to write the summary csv')
+parser.add_argument('--csv-path-summary-full', default=None, type=str,
+                    help='path to write the full summary csv')
+parser.add_argument('--csv-path-trained', default=None, type=str,
+                    help='path to write the train csv')
+parser.add_argument('--csv-path-validated', default=None, type=str,
                     help='path to write the validated csv')
+parser.add_argument('--model-path', default=None, type=str,
+                    help='the path to save the model')
 
 best_acc1 = 0
 
@@ -101,27 +125,28 @@ class ImageFolderWithPaths(datasets.ImageFolder):
 def main():
     args = parser.parse_args()
 
-    # prepare csv files
-    if args.write_csv_validated is not None:
-        csv_handler['csv_validated'] = getCSVHandler(
-            args.write_csv_validated,
-            'time required',
-            'file name',
-            'status',
-            'real class',
-            'predicted class 1',
-            'predicted class 1 (%)',
-            'predicted class 2',
-            'predicted class 2 (%)',
-            'predicted class 3',
-            'predicted class 3 (%)',
-            'predicted class 4',
-            'predicted class 4 (%)',
-            'predicted class 5',
-            'predicted class 5 (%)'
-        )
+    # write settings to csv file
+    if args.csv_path_settings is not None:
+        csv_handler['csv_file_settings'] = getCSVHandlerWithHeader(args.csv_path_settings, 'settings', False, args, time_start)
     else:
-        csv_handler['csv_validated'] = None
+        csv_handler['csv_file_settings'] = None
+
+    # prepare summary csv file
+    if args.csv_path_summary is not None:
+        csv_handler['csv_file_summary'] = getCSVHandlerWithHeader(args.csv_path_summary, 'summary', False, args, time_start)
+    else:
+        csv_handler['csv_file_summary'] = None
+
+    # prepare summary csv file
+    if args.csv_path_summary_full is not None:
+        csv_handler['csv_file_summary_full'] = getCSVHandlerWithHeader(args.csv_path_summary_full, 'summary_full', False, args, time_start)
+    else:
+        csv_handler['csv_file_summary_full'] = None
+
+    # write settings
+    if csv_handler['csv_file_settings'] is not None:
+        for prop in getClassProperties(args):
+            writeCSV(csv_handler['csv_file_settings'], False, prop, args.__dict__[prop])
 
     if args.seed is not None:
         random.seed(args.seed)
@@ -178,6 +203,11 @@ def main_worker(gpu, ngpus_per_node, args):
     else:
         print("=> creating model '{}'".format(args.arch))
         model = models.__dict__[args.arch]()
+
+    # set linear layer for output
+    if args.linear_layer is not None:
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, args.linear_layer)
 
     if args.distributed:
         # For multiprocessing distributed, DistributedDataParallel constructor
@@ -249,6 +279,8 @@ def main_worker(gpu, ngpus_per_node, args):
         transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
+            #transforms.CenterCrop(240),
+            #transforms.Resize([299, 299]),
             transforms.ToTensor(),
             normalize,
         ]))
@@ -264,6 +296,9 @@ def main_worker(gpu, ngpus_per_node, args):
 
     val_loader = torch.utils.data.DataLoader(
         ImageFolderWithPaths(valdir, transforms.Compose([
+            #transforms.CenterCrop(240),
+            #transforms.Resize([299, 299]),  # inception v3 input size: 299x299x3
+            #transforms.ToTensor(),
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
@@ -273,7 +308,7 @@ def main_worker(gpu, ngpus_per_node, args):
         num_workers=args.workers, pin_memory=True)
 
     if args.evaluate:
-        validate(val_loader, model, criterion, args)
+        validate(val_loader, model, criterion, args, checkpoint['epoch'])
         return
 
     for epoch in range(args.start_epoch, args.epochs):
@@ -285,7 +320,7 @@ def main_worker(gpu, ngpus_per_node, args):
         train(train_loader, model, criterion, optimizer, epoch, args)
 
         # evaluate on validation set
-        acc1 = validate(val_loader, model, criterion, args)
+        acc1 = validate(val_loader, model, criterion, args, epoch)
 
         # remember best acc@1 and save checkpoint
         is_best = acc1 > best_acc1
@@ -299,7 +334,7 @@ def main_worker(gpu, ngpus_per_node, args):
                 'state_dict': model.state_dict(),
                 'best_acc1': best_acc1,
                 'optimizer' : optimizer.state_dict(),
-            }, is_best)
+            }, is_best, args)
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
@@ -313,6 +348,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     model.train()
 
     end = time.time()
+    correct1_all = 0
+    time_all = 0
     for i, (input, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -328,6 +365,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        correct1, correct5 = accuracy_count(output, target, topk=(1, 5))
+        correct1_all += correct1
         losses.update(loss.item(), input.size(0))
         top1.update(acc1[0], input.size(0))
         top5.update(acc5[0], input.size(0))
@@ -340,19 +379,59 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
+        time_all += batch_time.val
 
+        # print output
         if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
+            print('Train ({top1.avg:.3f}%/{0:.3f}%): [{1}/{2}][{3}/{4}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
                   'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
+                  accuracy_best['train'], epoch + 1, args.epochs, i + 1, len(train_loader), batch_time=batch_time,
                    data_time=data_time, loss=losses, top1=top1, top5=top5))
 
+        # write full summary csv
+        if csv_handler['csv_file_summary_full'] is not None:
+            writeCSV(
+                csv_handler['csv_file_summary_full'],
+                False,
+                batch_time.val, # time taken
+                args.session_name, # train session (e.g. all)
+                epoch + 1, # epoch number current
+                args.epochs, # epoch number overall
+                'train', # current phase (train or val)
+                (i + 1) * args.batch_size, # current processed images
+                correct1_all, # correct processed images
+                len(train_loader) * args.batch_size, # images overall
+                (i + 1) * 100 / len(train_loader), # processed in percent
+                losses.avg, # loss
+                top1.avg, # accuracy in percent'
+                get_learning_rate_from_optimizer(optimizer) # learning rate
+            )
 
-def validate(val_loader, model, criterion, args):
+    # get best accuracy
+    accuracy_best['train'] = top1.avg if top1.avg > accuracy_best['train'] else accuracy_best['train']
+
+    # write summary csv
+    if csv_handler['csv_file_summary'] is not None:
+        writeCSV(
+            csv_handler['csv_file_summary'],
+            False,
+            time_all, # time taken
+            args.session_name, # train session
+            epoch + 1, # epoch number current
+            args.epochs, # epoch number overall
+            'train', # current phase (train or val)
+            losses.avg, # loss
+            top1.avg, # accuracy in percent
+            accuracy_best['train'], # best accuracy in percent'
+            get_learning_rate_from_optimizer(optimizer) # learning rate
+        )
+
+
+def validate(val_loader, model, criterion, args, epoch):
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -361,8 +440,18 @@ def validate(val_loader, model, criterion, args):
     # switch to evaluate mode
     model.eval()
 
+    # prepare summary csv file
+    if args.csv_path_validated is not None:
+        csv_handler['csv_file_validated'] = getCSVHandlerWithHeader(args.csv_path_validated, 'validated', epoch + 1, args,
+                                                                    time_start)
+    else:
+        csv_handler['csv_file_validated'] = None
+
     with torch.no_grad():
+
         end = time.time()
+        correct1_all = 0
+        time_all = 0
         for i, (input, target, path) in enumerate(val_loader):
 
             if args.gpu is not None:
@@ -379,8 +468,8 @@ def validate(val_loader, model, criterion, args):
 
             # measure accuracy and record loss
             acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            pred_percent, pred_class = predicted(output, 5, class_names)
-            target_class_name = class_names[int(target)]
+            correct1, correct5 = accuracy_count(output, target, topk=(1, 5))
+            correct1_all += correct1
 
             losses.update(loss.item(), input.size(0))
             top1.update(acc1[0], input.size(0))
@@ -389,9 +478,13 @@ def validate(val_loader, model, criterion, args):
             # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
+            time_all += batch_time.val
 
             if args.print_freq == 1 and args.batch_size == 1:
-                print('Test: [{0}/{1}]\t'
+                pred_percent, pred_class = predicted(output, 5, class_names)
+                target_class_name = class_names[int(target)]
+
+                print('Val ({top1.avg:.3f}%/{0:.3f}%): [{1}/{2}][{3}/{4}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                       'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
@@ -401,7 +494,7 @@ def validate(val_loader, model, criterion, args):
                       '{pred_class}\t'
                       '{pred_percent}\t'
                       '{real_class}'.format(
-                          i, len(val_loader), batch_time=batch_time, loss=losses,
+                          accuracy_best['val'], epoch + 1, args.epochs, i + 1, len(val_loader), batch_time=batch_time, loss=losses,
                           top1=top1, top5=top5, path=path[0],
                           correct='True' if pred_class[0] == target_class_name else 'False',
                           pred_class=','.join([str(x) for x in pred_class]),
@@ -410,9 +503,9 @@ def validate(val_loader, model, criterion, args):
                       )
                 )
 
-                if csv_handler['csv_validated'] is not None:
+                if csv_handler['csv_file_validated'] is not None:
                     writeCSV(
-                        csv_handler['csv_validated'],
+                        csv_handler['csv_file_validated'],
                         False,
                         batch_time.avg,
                         path[0],
@@ -430,24 +523,97 @@ def validate(val_loader, model, criterion, args):
                         pred_percent[4]
                     )
             elif i % args.print_freq == 0:
-                print('Test: [{0}/{1}]\t'
+                print('Val ({top1.avg:.3f}%/{0:.3f}%): [{1}/{2}][{3}/{4}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                       'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
                       'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       i, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
+                       accuracy_best['val'], epoch + 1, args.epochs, i + 1, len(val_loader), batch_time=batch_time,
+                       loss=losses, top1=top1, top5=top5))
+
+            # write full summary csv
+            if csv_handler['csv_file_summary_full'] is not None:
+                writeCSV(
+                    csv_handler['csv_file_summary_full'],
+                    False,
+                    batch_time.val,  # time taken
+                    args.session_name,  # train session (e.g. all)
+                    epoch + 1,  # epoch number current
+                    args.epochs,  # epoch number overall
+                    'val',  # current phase (train or val)
+                    (i + 1) * args.batch_size, # current processed images
+                    correct1_all,  # correct processed images
+                    len(val_loader) * args.batch_size,  # images overall
+                    (i + 1) * 100 / len(val_loader),  # processed in percent
+                    losses.avg,  # loss
+                    top1.avg,  # accuracy in percent'
+                    0 # learning rate
+                )
 
         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
               .format(top1=top1, top5=top5))
 
+        # get best accuracy
+        accuracy_best['val'] = top1.avg if top1.avg > accuracy_best['val'] else accuracy_best['val']
+
+        # write summary csv
+        if csv_handler['csv_file_summary'] is not None:
+            writeCSV(
+                csv_handler['csv_file_summary'],
+                False,
+                time_all, # time taken
+                args.session_name, # train session
+                epoch + 1, # epoch number current
+                args.epochs, # epoch number overall
+                'val', # current phase (train or val)
+                losses.avg, # loss
+                top1.avg, # accuracy in percent
+                accuracy_best['val'], # best accuracy in percent'
+                0 # learning rate
+            )
+
     return top1.avg
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
+def save_checkpoint(state, is_best, args, filename='checkpoint.pth.tar'):
+
+    # build the target paths
+    checkpoint_path = getFormatedPath(
+        args.model_path,
+        'checkpoint',
+        0,
+        time_start,
+        args.lr,
+        args.momentum,
+        args.batch_size,
+        args.workers,
+        args.weight_decay,
+        args.pretrained,
+        '{}_lr{}_m{}_bs{}_w{}_wd{}_{}.{}pth'
+    )
+
+    best_path = getFormatedPath(
+        args.model_path,
+        'model_best',
+        0,
+        time_start,
+        args.lr,
+        args.momentum,
+        args.batch_size,
+        args.workers,
+        args.weight_decay,
+        args.pretrained,
+        '{}_lr{}_m{}_bs{}_w{}_wd{}_{}.{}pth'
+    )
+
+    # prepare folder for saving the model
+    if args.model_path is not None:
+        createFolderForFile(checkpoint_path)
+
+    torch.save(state, checkpoint_path)
+
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+        shutil.copyfile(checkpoint_path, best_path)
 
 
 class AverageMeter(object):
@@ -469,8 +635,10 @@ class AverageMeter(object):
 
 
 def adjust_learning_rate(optimizer, epoch, args):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    """ Sets the learning rate to the initial LR by factor args.learning_rate_decrease_factor
+    every args.learning_rate_decrease_after epochs"""
+    lr = args.lr * (args.learning_rate_decrease_factor ** (epoch // args.learning_rate_decrease_after))
+
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
@@ -491,6 +659,24 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
+
+def accuracy_count(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(int(correct_k))
+        return res
+
+
 def predicted(output, topk, class_names):
     with torch.no_grad():
         pred_percent, pred_class = output.topk(topk, 1, True, True)
@@ -503,6 +689,12 @@ def predicted(output, topk, class_names):
             pred_class[idx] = class_names[class_id]
 
         return [pred_percent, pred_class]
+
+
+def get_learning_rate_from_optimizer(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
 
 if __name__ == '__main__':
     main()
